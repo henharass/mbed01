ac8fb22e9fe1ef48d5bc4ae5bc8b3b44feb8453b
henharass <s410201@my.cmsh.cyc.edu.tw>
2021-02-24 20:37:51 -0800

    add main.cpp

---
+++ mbed-os-test-lab1/main.cpp
@@ -0,0 +1,20 @@
+#include "mbed.h"
+
+DigitalOut myLED(LED3);
+DigitalOut myLED2(LED1);
+
+int main()
+{
+    myLED = 1;
+    myLED2 = 0;
+    while (1) {
+        for (int i = 0; i < 6; i++) {
+            myLED = !myLED;
+            ThisThread::sleep_for(1000ms);
+        }
+        for (int i = 0; i < 4; i++) {
+            myLED2 = !myLED2; 
+            ThisThread::sleep_for(1000ms);
+        } 
+    }
+}
